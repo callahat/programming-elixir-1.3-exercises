60a6f25550f0bc4f5951041fca49d0a798628635
callahat <tim.callahan25@yahoo.com>
2019-01-12 00:51:37 -0500

    sequence v 0.4.0

--- ch17/sequence/lib/sequence/server.ex
+++ ch17/sequence/lib/sequence/server.ex
@@ -2,7 +2,9 @@ defmodule Sequence.Server do
   use GenServer
   require Logger
 
-  @vsn 1
+  @vsn 2
+
+  # :sys.terminate(Sequence.Server, "because")
 
   defmodule State do
     defstruct current_number: 0, stash_pid: nil, delta: 1
@@ -26,8 +28,8 @@ defmodule Sequence.Server do
   # GenServer implementation
 
   def init(stash_pid) do
-    current_number = Sequence.Stash.get_value stash_pid
-    { :ok, %State{current_number: current_number, stash_pid: stash_pid} }
+    {current_number, delta} = Sequence.Stash.get_value stash_pid
+    { :ok, %State{current_number: current_number, stash_pid: stash_pid, delta: delta} }
   end
 
   def handle_call(:next_number, _from, state) do
@@ -48,15 +50,15 @@ defmodule Sequence.Server do
   end
 
   def terminate(_reason, state) do
-    Sequence.Stash.save_value state.stash_pid, state.current_number
+    Sequence.Stash.save_value state.stash_pid, {state.current_number, state.delta}
   end
 
-  def code_change(0, old_state = { current_number, stash_pid }, _extra) do
-    new_state = %State{current_number: current_number, stash_pid: stash_pid, delta: 1}
-    Logger.info "Changing code from 0 to 1"
-    Logger.info inspect(old_state)
-    Logger.info inspect(new_state)
-    {:ok, new_state}
+  def code_change(1, state = %{current_number: current_number, stash_pid: stash_pid, delta: delta}, _extra) do
+    Logger.info "Changing code from 1 to 2"
+    Logger.info inspect(Sequence.Stash.get_value(stash_pid))
+    Sequence.Stash.save_value stash_pid, {current_number, delta}
+    Logger.info inspect(Sequence.Stash.get_value(stash_pid))
+    {:ok, state}
   end
 
 end
