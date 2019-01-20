c74a9bab2baae57e06b688fb4b5cfb265793a5c4
callahat <tim.callahan25@yahoo.com>
2019-01-20 13:39:33 -0500

    Csv sigil ex2; parse numbers if they are alone

--- ch24/exes.exs
+++ ch24/exes.exs
@@ -30,9 +30,28 @@ defmodule CsvSigil do
     |> String.split("\n") 
     |> Enum.map(&String.split(&1,","))
     |> Enum.map(&_strip_lists(&1))
+    |> Enum.map(&_parse_numbers_in_list(&1))
   end
 
   defp _strip_lists(list) do
     list |> Enum.map(&String.trim(&1)) 
+  end
+
+  defp _parse_numbers_in_list(list) do
+    list |> Enum.map(&_parse_number(&1))
+  end
+
+  defp _parse_number(item) do
+    case Float.parse(item) do
+      :error      -> item
+      {float, ""} -> _as_int_if_int(float)
+      _           -> item
+    end
+  end
+
+  defp _as_int_if_int(number) do
+    if Float.floor(number) == number,
+    do:   trunc(number),
+    else: number
   end
 end
